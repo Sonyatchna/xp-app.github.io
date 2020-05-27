b8e1cae01bbcef5e2e1cd15d6afb547129fd23e8
Sonyatchna <sofiyatatchyn@gmail.com>
2020-05-28 01:31:38 +0300

    main commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'news',
+    pathMatch: 'full'
+  },
+  {
+    path: 'news',
+    loadChildren: './modules/news/news.module#NewsModule'
+  },
+  {
+    path: 'news/:id',
+    loadChildren: './modules/news-by-id/news-by-id.module#NewsByIdModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
